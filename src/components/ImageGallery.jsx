4d671d9e2e81d853c7b15c4ea20a0c06021bc865
Tahir Yoga <tahiralauddin7@gmail.com>
2024-07-15 19:24:01 +0500

    fix: absolute paths of porfolio changed to relative paths

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -10,11 +10,11 @@ const ImageGallery = ({ projectId }) => {
       "title": "All Book Quotes",
       "description": "A Directory of all the books",
       "images": [
-        "src/assets/img/Portfolio/Allbooksquotes Portfolio/home-page.png",
-        "src/assets/img/Portfolio/Allbooksquotes Portfolio/quotes-page-related-page.png",
-        "src/assets/img/Portfolio/Allbooksquotes Portfolio/home-page-books.png",
-        "src/assets/img/Portfolio/Allbooksquotes Portfolio/quotes-page1.png",
-        "src/assets/img/Portfolio/Allbooksquotes Portfolio/quotes-page2.png",
+        "/assets/img/Portfolio/Allbooksquotes Portfolio/home-page.png",
+        "/assets/img/Portfolio/Allbooksquotes Portfolio/quotes-page-related-page.png",
+        "/assets/img/Portfolio/Allbooksquotes Portfolio/home-page-books.png",
+        "/assets/img/Portfolio/Allbooksquotes Portfolio/quotes-page1.png",
+        "/assets/img/Portfolio/Allbooksquotes Portfolio/quotes-page2.png",
       ]
     },
     {
@@ -22,13 +22,13 @@ const ImageGallery = ({ projectId }) => {
       "title": "AspectGaming",
       "description": "Your All in One Gaming Software",
       "images": [
-        "src/assets/img/Portfolio/AspectGaming/home.png",
-        "src/assets/img/Portfolio/AspectGaming/Cleanup.png",
-        "src/assets/img/Portfolio/AspectGaming/Contact Us Page.png",
-        "src/assets/img/Portfolio/AspectGaming/GPU page.png",
-        "src/assets/img/Portfolio/AspectGaming/License Page.png",
-        "src/assets/img/Portfolio/AspectGaming/Memory Tweaks.png",
-        "src/assets/img/Portfolio/AspectGaming/Mouse Keyboard Tweaks.png",       
+        "/assets/img/Portfolio/AspectGaming/home.png",
+        "/assets/img/Portfolio/AspectGaming/Cleanup.png",
+        "/assets/img/Portfolio/AspectGaming/Contact Us Page.png",
+        "/assets/img/Portfolio/AspectGaming/GPU page.png",
+        "/assets/img/Portfolio/AspectGaming/License Page.png",
+        "/assets/img/Portfolio/AspectGaming/Memory Tweaks.png",
+        "/assets/img/Portfolio/AspectGaming/Mouse Keyboard Tweaks.png",       
       ]
     },
     {
@@ -36,12 +36,12 @@ const ImageGallery = ({ projectId }) => {
       "title": "Instagnite",
       "description": "Instagram Tools at one place",
       "images": [
-        "src/assets/img/Portfolio/Instagnite/account-details.PNG",
-        "src/assets/img/Portfolio/Instagnite/actions.PNG",
-        "src/assets/img/Portfolio/Instagnite/advance-options.PNG",
-        "src/assets/img/Portfolio/Instagnite/bot-options.PNG",
-        "src/assets/img/Portfolio/Instagnite/comments-page.PNG",
-        "src/assets/img/Portfolio/Instagnite/story-options-page.PNG",
+        "/assets/img/Portfolio/Instagnite/account-details.PNG",
+        "/assets/img/Portfolio/Instagnite/actions.PNG",
+        "/assets/img/Portfolio/Instagnite/advance-options.PNG",
+        "/assets/img/Portfolio/Instagnite/bot-options.PNG",
+        "/assets/img/Portfolio/Instagnite/comments-page.PNG",
+        "/assets/img/Portfolio/Instagnite/story-options-page.PNG",
       ]
     },
     {
@@ -49,29 +49,29 @@ const ImageGallery = ({ projectId }) => {
       "title": "Konnected Reverse Raffle",
       "description": "A fun to play Game with friends",
       "images": [
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-dashboard.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/invalid-license-key.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-dashboard.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-license.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-user-list.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-aqua-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-black-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-come-back.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-eliminated.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-final-10.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-come-back.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-eliminated.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-final-10.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-pink-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-purple-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-come-back.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-eliminated.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-final-10.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-yellow-grid.PNG",
-        "src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/valid-license-key.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-dashboard.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/invalid-license-key.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-dashboard.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-license.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-admin-user-list.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-aqua-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-black-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-come-back.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-eliminated.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-final-10.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-flower-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-come-back.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-eliminated.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-final-10.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-industrial-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-pink-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-purple-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-come-back.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-eliminated.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-final-10.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-sunset-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/krr-home-yellow-grid.PNG",
+        "/assets/img/Portfolio/KonnectedReverseRaffle/Pics/valid-license-key.PNG",
       ]
     },
     {
@@ -79,13 +79,13 @@ const ImageGallery = ({ projectId }) => {
       "title": "Vital AI",
       "description": "HealthCare Management System",
       "images": [
-        "src/assets/img/Portfolio/Vital AI/home.png",
-        "src/assets/img/Portfolio/Vital AI/appointments.png",
-        "src/assets/img/Portfolio/Vital AI/diagnoses.png",
-        "src/assets/img/Portfolio/Vital AI/patients-detail.png",
-        "src/assets/img/Portfolio/Vital AI/patients.png",
-        "src/assets/img/Portfolio/Vital AI/questionnaire.png",
-        "src/assets/img/Portfolio/Vital AI/treatment-page.png",
+        "/assets/img/Portfolio/Vital AI/home.png",
+        "/assets/img/Portfolio/Vital AI/appointments.png",
+        "/assets/img/Portfolio/Vital AI/diagnoses.png",
+        "/assets/img/Portfolio/Vital AI/patients-detail.png",
+        "/assets/img/Portfolio/Vital AI/patients.png",
+        "/assets/img/Portfolio/Vital AI/questionnaire.png",
+        "/assets/img/Portfolio/Vital AI/treatment-page.png",
       ]
     },
     {
@@ -93,15 +93,15 @@ const ImageGallery = ({ projectId }) => {
       "title": "Jewel",
       "description": "A Jewelery Store Web App",
       "images": [
-        "src/assets/img/Portfolio/JewelBox/login.png",
-        "src/assets/img/Portfolio/JewelBox/add-customer.png",
-        "src/assets/img/Portfolio/JewelBox/customers.png",
-        "src/assets/img/Portfolio/JewelBox/invoice-detail.png",
-        "src/assets/img/Portfolio/JewelBox/invoices.png",
-        "src/assets/img/Portfolio/JewelBox/order-detail-1.png",
-        "src/assets/img/Portfolio/JewelBox/order-detail-2.png",
-        "src/assets/img/Portfolio/JewelBox/order-detail-3.png",
-        "src/assets/img/Portfolio/JewelBox/orders.png",
+        "/assets/img/Portfolio/JewelBox/login.png",
+        "/assets/img/Portfolio/JewelBox/add-customer.png",
+        "/assets/img/Portfolio/JewelBox/customers.png",
+        "/assets/img/Portfolio/JewelBox/invoice-detail.png",
+        "/assets/img/Portfolio/JewelBox/invoices.png",
+        "/assets/img/Portfolio/JewelBox/order-detail-1.png",
+        "/assets/img/Portfolio/JewelBox/order-detail-2.png",
+        "/assets/img/Portfolio/JewelBox/order-detail-3.png",
+        "/assets/img/Portfolio/JewelBox/orders.png",
       ]
     },
     {
@@ -109,16 +109,16 @@ const ImageGallery = ({ projectId }) => {
       "title": "Starc Assistant",
       "description": "A Jewellery Store Web App",
       "images": [
-        "src/assets/img/Portfolio/Starc Assistant/login.png",
-        "src/assets/img/Portfolio/Starc Assistant/home.png",
-        "src/assets/img/Portfolio/Starc Assistant/chatbot-interface.png",
-        "src/assets/img/Portfolio/Starc Assistant/machines-detail.png",
-        "src/assets/img/Portfolio/Starc Assistant/machines-list.png",
-        "src/assets/img/Portfolio/Starc Assistant/previous-page-topics.png",
-        "src/assets/img/Portfolio/Starc Assistant/topic-detail-page.png",
-        "src/assets/img/Portfolio/Starc Assistant/tornitura-bot.png",
-        "src/assets/img/Portfolio/Starc Assistant/training-list-admin.png",
-        "src/assets/img/Portfolio/Starc Assistant/welcome.png",
+        "/assets/img/Portfolio/Starc Assistant/login.png",
+        "/assets/img/Portfolio/Starc Assistant/home.png",
+        "/assets/img/Portfolio/Starc Assistant/chatbot-interface.png",
+        "/assets/img/Portfolio/Starc Assistant/machines-detail.png",
+        "/assets/img/Portfolio/Starc Assistant/machines-list.png",
+        "/assets/img/Portfolio/Starc Assistant/previous-page-topics.png",
+        "/assets/img/Portfolio/Starc Assistant/topic-detail-page.png",
+        "/assets/img/Portfolio/Starc Assistant/tornitura-bot.png",
+        "/assets/img/Portfolio/Starc Assistant/training-list-admin.png",
+        "/assets/img/Portfolio/Starc Assistant/welcome.png",
       ]
     }
    
@@ -140,7 +140,7 @@ const ImageGallery = ({ projectId }) => {
     <div className='image-gallery-parent'>
     <div className="image-gallery">
       <div className="large-image">
-        <img src={largeImage || project.images[0]} alt="Large" />
+        <img ={largeImage || project.images[0]} alt="Large" />
       </div>
       <div className="project-details">
         <h2>{project.title}</h2>
@@ -151,7 +151,7 @@ const ImageGallery = ({ projectId }) => {
           {project.images.map((image, index) => (
             <img
               key={index}
-              src={image}
+              ={image}
               alt={`Small ${index + 1}`}
               onClick={() => setLargeImage(image)}
               />
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -9,80 +9,17 @@ import EmblaCarousel from './Embla/EmblaCarousel'
 
 function Portfolio() {
   const items: {[key: string]: string} = {
-    'konnected': 'src/assets/img/Portfolio/KonnectedReverseRaffle/Pics/invalid-license-key.PNG',
-    'loote': 'src/assets/img/Portfolio/Loote/email marketing.png',
-    'vital-ai': 'src/assets/img/Portfolio/Vital AI/home.PNG',
-    'aspect-gaming': 'src/assets/img/Portfolio/AspectGaming/home.png',
-    'recipito': 'src/assets/img/Portfolio/Recipe/home.png',
+    'konnected': '/assets/img/Portfolio/KonnectedReverseRaffle/Pics/invalid-license-key.PNG',
+    'loote': '/assets/img/Portfolio/Loote/email marketing.png',
+    'vital-ai': '/assets/img/Portfolio/Vital AI/home.PNG',
+    'aspect-gaming': '/assets/img/Portfolio/AspectGaming/home.png',
+    'recipito': '/assets/img/Portfolio/Recipe/home.png',
   }
     const OPTIONS = {}
 
 
   return (
     <EmblaCarousel slides={items} options={OPTIONS} />
-
-    // <div id='portfolio' className="portfolio">
-    //   <h1>My Portfolio</h1>
-    //   <p>Showcasing my web development projects and achievements.</p>
-    //   <div className="embla" ref={emblaRef}>
-    //     <div className="portfolio-items embla__container">
-    //         <div className="portfolio-item embla__slide" onClick={() => openPopup('konnected')}>
-    //           <img src="src\assets\img\Portfolio\KonnectedReverseRaffle\Pics\invalid-license-key.PNG" alt="Konnected Reverse Raffle" />
-    //           <h3>Konnected Reverse Raffle</h3>
-    //           <span>2023-2023</span>
-    //         </div>
-      
-    //         <div className="portfolio-item embla__slide" onClick={() => openPopup('loote')}>
-    //               <img src="src\assets\img\Portfolio\Loote\email marketing.png" alt="Loote" />
-    //               <h3>Loote</h3>
-    //               <span>2023-2023</span>
-    //           </div>
-    //           <div className="portfolio-item embla__slide" onClick={() => openPopup('vital-ai')}>
-    //               <img src="src\assets\img\Portfolio\Vital AI\home.PNG" alt="Vital AI" />
-    //               <h3>Vital AI</h3>
-    //               <span>2023-2023</span>
-    //           </div>
-    //           <div className="portfolio-item embla__slide" onClick={() => openPopup('aspect-gaming')}>
-    //             <img
-    //               src="src/assets/img/Portfolio/AspectGaming/home.png"
-    //               alt="Project Image"
-    //             />
-    //             <h3>Aspect Gaming</h3>
-    //             <span>2023-2023</span>
-    //           </div>
-    //           <div className="portfolio-item embla__slide" onClick={() => openPopup('recipito')}>
-    //             <img src="src\assets\img/Portfolio/Recipe/home.png" alt="Project Image" />
-    //             <h3>Recipito</h3>
-    //           <span>2023-2023</span>
-    //           </div>
-    //     </div>
-    //     <div className="embla__controls">
-    //     <div className="embla__buttons">
-    //       <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-    //       <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-    //     </div>
-
-    //     <div className="embla__dots">
-    //       {scrollSnaps.map((_, index) => (
-    //         <DotButton
-    //           key={index}
-    //           onClick={() => onDotButtonClick(index)}
-    //           className={'embla__dot'.concat(
-    //             index === selectedIndex ? ' embla__dot--selected' : ''
-    //           )}
-    //         />
-    //       ))}
-    //     </div>
-    //   </div>
-    //   </div>
-
-    //   <div id="popup" className="popup">
-    //     <div className="popup-content">
-    //       <span className="close" onClick={() => closePopup()}>&times;</span>
-    //       <div id="popup-inner-content"></div>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
