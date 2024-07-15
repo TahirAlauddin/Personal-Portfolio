2e561aca93d91271072aad7f0e50e44b636f6b13
Tahir Yoga <tahiralauddin7@gmail.com>
2024-07-15 19:29:03 +0500

    fix: testimonials images path are now relative

--- src/utils/Testimonials.ts
+++ src/utils/Testimonials.ts
@@ -7,49 +7,49 @@ interface Testimonial {
   
 const Testimonials: Testimonial[] = [
   {
-    imgSrc: "src/assets/img/Testimonials/Chandhan.png",
+    imgSrc: "/assets/img/Testimonials/Chandhan.png",
     clientName: "Chandhan",
     clientLocation: "India",
     comment: "He is a true professional and knows his stuff very well. He created an amazing website for me and is always available to support you in case you require any help or modification. I would definitely recommend his services to others and would be interested in future projects."
   },
   {
-    imgSrc: "src/assets/img/Testimonials/Basit.png",
+    imgSrc: "/assets/img/Testimonials/Basit.png",
     clientName: "Basit",
     clientLocation: "Malaysia",
     comment: "Its really wonderful to work with this guy. He is professional and hardworking. Go beyond expectation in work. I highly recommend anyone who has Coding related work give it to he is an excellent programmer with best communication skills. Everything work smoothly as expected."
   },
   {
-    imgSrc: "src/assets/img/Testimonials/Shirine_hegazi.png",
+    imgSrc: "/assets/img/Testimonials/Shirine_hegazi.png",
     clientName: "Shirine Hegazi",
     clientLocation: "United States",
     comment: "Very talented and professional. I was impressed with how Tahir had a good understanding of the project and asked all the right questions to things we hadn't even thought of, which of course, ended in a high end polished software for our practice. Highly recommend!"
   },
   {
-    imgSrc: "src/assets/img/Testimonials/Lindsaym.png",
+    imgSrc: "/assets/img/Testimonials/Lindsaym.png",
     clientName: "Lindsaym",
     clientLocation: "United States",
     comment: "I HIGHLY recommend this seller. There was a fast turnaround time with the software, communication daily and support once the software was complete. The seller has high standards for themselves to insure a product that works flawlessly. Will certainly return! Thank You!!"
   },
   {
-    imgSrc: "src/assets/img/Testimonials/Paul.png",
+    imgSrc: "/assets/img/Testimonials/Paul.png",
     clientName: "Paul",
     clientLocation: "United Kingdom",
     comment: "Great service. Accurate and timely."
   },
   {
-    imgSrc: "src/assets/img/Testimonials/Russel.png",
+    imgSrc: "/assets/img/Testimonials/Russel.png",
     clientName: "Russel",
     clientLocation: "United States",
     comment: "Excellent job sir! This is just what I needed. Nice clean design and very effective program. Thank you for the quick turn around. Good coder."
   },
   {
-    imgSrc: "src/assets/img/Testimonials/chris-walsh.jpg",
+    imgSrc: "/assets/img/Testimonials/chris-walsh.jpg",
     clientName: "Chris Walsh",
     clientLocation: "Canada",
     comment: "Amazing got to work right away is a professional, a great communicator. and we are going to work again soon, awesome would highly recommend."
   },
   {
-    imgSrc: "src/assets/img/Testimonials/moopsten.jpg",
+    imgSrc: "/assets/img/Testimonials/moopsten.jpg",
     clientName: "Charles Rice",
     clientLocation: "United States",
     comment: "Tahir is always amazing, responsive and delivers quality work."
